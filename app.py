dd34b45871380491e6450152e9f139f0ddb4f6ea
Pooja <pkale@purdue.edu>
2022-12-09 18:21:11 -0800

    Add error codes to APIS

--- app.py
+++ app.py
@@ -49,21 +49,9 @@ WHERE
 GROUP BY
 	c.investors""")
 
-GET_INVESTORS_ORM = ("""
-SELECT c.investors FROM companies c
-	LEFT JOIN company_li_names ON c.company_name = company_li_names.company_name
-WHERE 
-	company_li_names.company_li_name =:company_li_name and c.investors is not null
-GROUP BY
-	c.investors; VALUES (:company_li_name)""")
-
 GET_COMPANIES = ("""
 SELECT people.company_name FROM people 
 WHERE person_id =(%s); """) 
-
-GET_COMPANIES_ORM = ("""
-SELECT people.company_name FROM people 
-WHERE person_id =:person_id; VALUES(:person_id)""") 
 
 
 GET_AVG_FUNDING_BY_PERSON = ("""
@@ -78,71 +66,82 @@ WHERE (companies.company_name = people.company_name
 AND person_id = (%s)
 """)
 
-GET_AVG_FUNDING_BY_PERSON_ORM = ("""
-SELECT
-	avg(companies.known_total_funding) as avg
-FROM
-	people
-	INNER JOIN company_li_names ON people.company_li_name = company_li_names.company_li_name
-	INNER JOIN companies ON company_li_names.company_name = companies.company_name
-WHERE (companies.company_name = people.company_name
-	OR company_li_names.company_li_name = people.company_li_name)
-AND person_id = :person_id; VALUES(:person_id)
-""")
 
 
 @app.get('/')
 def home(): 
     return ('Hello World!')
 
-@app.get('/companies-by-person')
-def get_companies_by_person():
-
-    data = request.get_json()
-    person_id = data['person_id']
+@app.get('/companies-by-person/<given_person_id>')
+def get_companies_by_person(given_person_id):
+    person_id = None
+    if given_person_id: 
+        person_id = given_person_id
+    else: 
+        data = request.get_json()
+        person_id = data['person_id']
     try:
         uuid.UUID(str(person_id))
     except ValueError:
-        return {"statusCode": 400, "message": 'Invalid uuid person_id: ' +person_id}
+        return {"statusCode": 400, "Errors" : {"message": 'Bad Request: Invalid uuid person_id: ' +person_id}}
+    
+    exists = db.session.query(models.Person.person_id).filter_by(person_id=person_id).first() is not None
+    if not exists: 
+            return { "investors": [], "statusCode":404, "Errors": {"Message": "Not found: Person_id does not exist in the db."}}
+    
     with connection: 
         with connection.cursor() as cursor: 
             cursor.execute(GET_COMPANIES,(person_id,))
             companies = [item for sublist in cursor.fetchall() for item in sublist]
-    return {"statusCode":200, "companies":companies, "message":'Successfully completed the request for person_id '+person_id}
+    return {"statusCode":200, "Response":{"companies":companies}, "Errors": {}}
 
 
 
-@app.get('/avg-funding-by-person')
-def get_average_funding_by_person(): 
-    try:
-        data = request.get_json() 
-    except:
-        return {"statusCode": 400, "message":"Invalid number of parameters."}
-    person_id = data['person_id']
+@app.get('/avg-funding-by-person/<given_person_id>')
+def get_average_funding_by_person(given_person_id): 
+    person_id = None
+    if given_person_id: 
+        person_id = given_person_id
+    else: 
+        try:
+            data = request.get_json() 
+            person_id = data['person_id']
+        except:
+            return {"statusCode": 400, "message":"Bad Request: Invalid number of parameters."}
     try:
         uuid.UUID(str(person_id))
     except ValueError:
-        return {"statusCode": 400, "message": 'Invalid uuid person_id: ' +person_id}   
+        return {"statusCode": 400, "Errors": { "message": 'Invalid uuid person_id: ' +person_id}}
+    
+    exists = db.session.query(models.Person.person_id).filter_by(person_id=person_id).first() is not None
+    if not exists: 
+            return { "investors": [], "statusCode":404, "Errors": {"Message": "Not Found: Person_id does not exist in the db."}}
+    
     with connection: 
         with connection.cursor() as cursor: 
             cursor.execute(GET_AVG_FUNDING_BY_PERSON,(person_id,))
             avg_funding = cursor.fetchone()[0]
-    return {"statusCode":200, "average_funding":avg_funding, "message":'Successfully completed the request for person_id '+person_id}
+
+    return {"statusCode":200, "Response":{"avg_funding":avg_funding}, "Errors":{}}
 
 
 
-@app.get('/investors-by-company')
-def get_investors_by_company_li_name():
-    try:
-        data = request.get_json() 
-    except:
-        return {"statusCode": 400, "message": 'Incorrection number of parameters.' }
-    company_li_name = data['company_li_name']
+@app.get('/investors-by-company/<given_company_li_name>')
+def get_investors_by_company_li_name(given_company_li_name):
+    company_li_name = None
+    if given_company_li_name: 
+        company_li_name = given_company_li_name
+    exists = db.session.query(models.Company_Li_Mapping.company_name).filter_by(company_li_name=company_li_name).first() is not None
+    if exists is None: 
+            return { "Response: ": [], "statusCode":404, "Errors": {"Message": "Not Found: Company_li_name does not exist in the db."}}
     with connection: 
         with connection.cursor() as cursor: 
             cursor.execute(GET_INVESTORS, (company_li_name,))
-            investors = cursor.fetchone()[0]
-            investors = investors.replace('\n','').replace('"','').replace(' ','').lstrip('[').lstrip(']').split(',')
-    return {"investors": investors}
+            investors = cursor.fetchone()
+            if investors is None: 
+                investors = []
+            else: 
+                investors = investors[0].replace('\n','').replace('"','').replace(' ','').lstrip('[').lstrip(']').split(',')
+    return { "Response": {"investors":investors}, "statusCode":200, "Errors": {}}
 
 
